6df3c97ce5b29a6f63a32fb2c8712f7638891fbd
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 19:22:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function happyHolidaysTo(days, holiday){
+function holidayCountdown(days, holiday){
   return `It's ${days} days until ${holiday}!`
 }
